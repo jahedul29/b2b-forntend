546b37a586f2b5244e4661416d3b879cd17dc71d
jahedul29 <jahedulh1@gmail.com>
2021-02-16 18:27:48 +0600

    change in scss

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import loginStyle from '../styles/Login.module.scss'
+
+const Login = () => {
+    return (
+        <div className={loginStyle.auth_container}>
+            <div className={loginStyle.auth_form_container}>
+        
+            </div>
+        </div>
+    );
+};
+
+export default Login;
